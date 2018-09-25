295236070bd845d4b001b8ab1076b7e339468d4d
shayne preston <pandemic2all@gmail.com>
2018-09-24 20:57:02 -0400

    test

--- app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
+++ app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.prests1.rushmeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//blah
 public class MainActivity extends AppCompatActivity {
 
     @Override
