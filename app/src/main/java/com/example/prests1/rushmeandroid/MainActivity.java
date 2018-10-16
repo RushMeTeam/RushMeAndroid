2822495aab676a1997777c6d9001e141081c5c82
shayne preston <pandemic2all@gmail.com>
2018-10-16 13:20:46 -0400

    Trying to change app icon

--- app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
+++ app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.prests1.rushmeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//blahh
+import android.support.v7.app.ActionBar;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
     }
 }
