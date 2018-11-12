028902210e191c8689de0c596ca1aa448000d4a5
Harry Rubenzahl <me@harry.im>
2018-11-11 20:28:32 -0500

    Unused imports.

--- app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
+++ app/src/main/java/com/example/prests1/rushmeandroid/MainActivity.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 LinearLayout parent;
