43af354e7742716a1948d9f0221dc8b184c96eaf
shayne preston <pandemic2all@gmail.com>
2019-03-15 17:58:09 -0400

    commenting for ActionLogging.java

--- app/src/main/java/com/example/prests1/rushmeandroid/ActionLogging.java
+++ app/src/main/java/com/example/prests1/rushmeandroid/ActionLogging.java
@@ -2,8 +2,22 @@ package com.example.prests1.rushmeandroid;
 
 import android.util.Log;
 
+/**
+ * User Action Logging to be stored for analytic use
+ */
 public class ActionLogging {
 
+    /**
+     * This is the logging function that takes in an event which is a string of a list of event types and an optional detail about
+     * what is happening in the event
+     *
+     * i.e.
+     *  event = Fraternity Selected
+     *  detail = Fraternity.getName()
+     *
+     * @param event <-- predefined events that can happen
+     * @param detail <-- optional details from the event
+     */
     public static void Log(String event, String detail){
         switch(event){
             case "Fraternity Selected":
