837d56058b6bd4486363cf02e287d18a568aa23b
Xtreme <darriangardea@gmail.com>
2019-03-29 13:31:03 -0400

    Implemented Splash Screen
    The app now starts by fading into the white RushMe logo on a light blue background.

---
+++ app/src/main/java/com/example/prests1/rushmeandroid/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.prests1.rushmeandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Sumeet Jain on 23-06-2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.appsplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
